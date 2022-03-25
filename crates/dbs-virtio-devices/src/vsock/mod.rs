f4fa0aae998137187b1c92edcb002fd041ae8234
Beiyue <beiyue@linux.alibaba.com>
2022-03-25 18:36:54 +0800

    dbs-virtio-devices: add vsock packet.
    Add VsockPacket, which provides a thin wrapper over the buffers used by
    vsock.
    
    Signed-off-by: Beiyue <beiyue@linux.alibaba.com>

--- crates/dbs-virtio-devices/src/vsock/mod.rs
+++ crates/dbs-virtio-devices/src/vsock/mod.rs
@@ -6,12 +6,44 @@
 // found in the THIRD-PARTY file.
 
 pub mod backend;
+mod packet;
+
+use vm_memory::GuestMemoryError;
+
+mod defs {
+    /// Max vsock packet data/buffer size.
+    pub const MAX_PKT_BUF_SIZE: usize = 64 * 1024;
+}
 
 #[derive(Debug, thiserror::Error)]
 pub enum VsockError {
     /// vsock backend error
     #[error("Vsock backend error: {0}")]
     Backend(#[source] std::io::Error),
+    /// The vsock data/buffer virtio descriptor is expected, but missing.
+    #[error("The vsock data/buffer virtio descriptor is expected, but missing")]
+    BufDescMissing,
+    /// The vsock data/buffer virtio descriptor length is smaller than expected.
+    #[error("The vsock data/buffer virtio descriptor length is smaller than expected")]
+    BufDescTooSmall,
+    /// Chained GuestMemory error.
+    #[error("Chained GuestMemory error: {0}")]
+    GuestMemory(#[source] GuestMemoryError),
+    /// Bounds check failed on guest memory pointer.
+    #[error("Bounds check failed on guest memory pointer, addr: {0}, size: {1}")]
+    GuestMemoryBounds(u64, usize),
+    /// The vsock header descriptor length is too small.
+    #[error("The vsock header descriptor length {0} is too small")]
+    HdrDescTooSmall(u32),
+    /// The vsock header `len` field holds an invalid value.
+    #[error("The vsock header `len` field holds an invalid value {0}")]
+    InvalidPktLen(u32),
+    /// Encountered an unexpected write-only virtio descriptor.
+    #[error("Encountered an unexpected write-only virtio descriptor")]
+    UnreadableDescriptor,
+    /// Encountered an unexpected read-only virtio descriptor.
+    #[error("Encountered an unexpected read-only virtio descriptor")]
+    UnwritableDescriptor,
 }
 
 type Result<T> = std::result::Result<T, VsockError>;
